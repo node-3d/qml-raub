50890c31a09e707c6f01d8caa22c127114f6398a
Luis Blanco <raubtierxxx@gmail.com>
2017-02-14 15:49:53 +0300

    fix path

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ require('node-deps-qt-qml-raub');
 const os = require('os');
 
 const addonPaths = {
-	win32 : '../bin_win32' ,
-	linux : '../bin_linux' ,
-	darwin: '../bin_darwin',
+	win32 : 'bin_win32' ,
+	linux : 'bin_linux' ,
+	darwin: 'bin_darwin',
 };
 
 process.env.path += ';' + __dirname + addonPaths[os.platform()];
