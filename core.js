3e1467566f5e1a9fc99fde5ba8cd21d1ff45421b
Luis Blanco <raubtierxxx@gmail.com>
2018-09-08 20:02:58 +0300

    :fire: Remove debug

--- core.js
+++ core.js
@@ -11,12 +11,10 @@ const { binPath } = require('addon-tools-raub');
 
 const core = require(`./${binPath}/qml`);
 
-console.log('core.js', 'PLUGINS');
+
 core.View.plugins(`${depCore.binPath}/plugins`);
 core.View.plugins(`${depGui.binPath}/plugins`);
 core.View.plugins(`${depQml.binPath}/plugins`);
-console.log('core.js', `${depCore.binPath}/plugins`);
-console.log('core.js', `${depGui.binPath}/plugins`);
-console.log('core.js', `${depQml.binPath}/plugins`);
+
 
 module.exports = core;
