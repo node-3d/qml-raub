c18090c76d9821dcaf796c1c90f6c261c86303a4
Luis Blanco <luisblanco1337@gmail.com>
2019-09-26 15:11:38 +0300

    :hammer: Use rtld_now

--- src/cpp/view.cpp
+++ src/cpp/view.cpp
@@ -117,24 +117,24 @@ JS_METHOD(View::init) {
 	
 	// Preload the libs with OUR @RPATH, not some junk builtin rpaths
 	#ifdef __linux__
-	dlopen("libicui18n.so.56", RTLD_LAZY);
-	dlopen("libicuuc.so.56", RTLD_LAZY);
-	dlopen("libicudata.so.56", RTLD_LAZY);
-	dlopen("libicuio.so.56", RTLD_LAZY);
-	dlopen("libicule.so.56", RTLD_LAZY);
-	dlopen("libicutu.so.56", RTLD_LAZY);
-	dlopen("libQt5Core.so.5", RTLD_LAZY);
-	dlopen("libQt5Network.so.5", RTLD_LAZY);
-	dlopen("libQt5DBus.so.5", RTLD_LAZY);
-	dlopen("libQt5Gui.so.5", RTLD_LAZY);
-	dlopen("libQt5OpenGL.so.5", RTLD_LAZY);
-	dlopen("libQt5Widgets.so.5", RTLD_LAZY);
-	dlopen("libQt5XcbQpa.so.5", RTLD_LAZY);
-	dlopen("libQt5Qml.so.5", RTLD_LAZY);
-	dlopen("libQt5Quick.so.5", RTLD_LAZY);
-	dlopen("libQt5QuickControls2.so.5", RTLD_LAZY);
-	dlopen("libQt5QuickTemplates2.so.5", RTLD_LAZY);
-	dlopen("libQt5QuickWidgets.so.5", RTLD_LAZY);
+	dlopen("libicui18n.so.56", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libicuuc.so.56", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libicudata.so.56", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libicuio.so.56", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libicule.so.56", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libicutu.so.56", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5Core.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5Network.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5DBus.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5Gui.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5OpenGL.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5Widgets.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5XcbQpa.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5Qml.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5Quick.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5QuickControls2.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5QuickTemplates2.so.5", RTLD_NOW | RTLD_GLOBAL);
+	dlopen("libQt5QuickWidgets.so.5", RTLD_NOW | RTLD_GLOBAL);
 	#endif
 	
 	QmlUi::init(cwdOwn.c_str(), wnd, ctx, commonCb);
