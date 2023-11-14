91fcfb9d963ce61c28e37466df2c0d1709d3db24
Luis Blanco <luisblanco1337@gmail.com>
2023-11-14 22:42:52 +0400

    Adjust d.ts for Method

--- index.d.ts
+++ index.d.ts
@@ -242,8 +242,12 @@ declare module "qml-raub" {
 		key: string;
 	}>;
 	
+	type TUnknownFn = (
+		a0?: any, a1?: any, a2?: any, a3?: any, a4?: any,
+		a5?: any, a6?: any, a7?: any, a8?: any, a9?: any,
+	) => unknown;
 	interface TNewableMethod {
-		new(opts: TOptsMethod): (...args: unknown[]) => unknown;
+		new(opts: TOptsMethod): TUnknownFn;
 	}
 	
 	/**
